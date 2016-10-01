081197966643164f82486765045ec2729714fbcb
Carolina Alvarez <caralvarez@uymo-caralvarez.guest.fing.edu.uy>
2016-10-01 14:28:30 -0300

    refactor code

--- src/Main.java
+++ src/Main.java
@@ -5,41 +5,53 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try{
 
-            String host = "www.twitter.com";
-            //String host = "www.metroflog.com";
+        String host = "www.google.com.uy";
+        //connectToHost(host,"1.0");
+        //connectToHost(host,"1.1");
+        connectToHost(host,"2.0");
 
-            Socket socket = new Socket( host, 443);
+    }
 
-            PrintStream out = new PrintStream( socket.getOutputStream() );
-            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
+    private static void  connectToHost(String host, String protocol) {
+        try {
+            Socket socket = new Socket(host, 80);
 
-            out.println("GET / HTTP/1.1");
+            PrintStream out = new PrintStream(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            if(protocol.equalsIgnoreCase("1.0")){
+                out.println("GET / HTTP/1.0");
+            }else{
+                out.println("GET / HTTP/1.1");
+            }
             out.println("Host: " + host);
-            out.println("Connection: Upgrade, HTTP2-Settings");
-            out.println( "Upgrade: h2c");
-            out.println( "HTTP2-Settings: <base64url encoding of HTTP/2 SETTINGS payload>");
 
+            if(protocol.equalsIgnoreCase("2.0")){
+                out.println("Connection: Upgrade, HTTP2-Settings");
+                out.println( "Upgrade: h2c");
+                out.println( "HTTP2-Settings: <base64url encoding of HTTP/2 SETTINGS payload>");
+            }
             out.println();
             out.flush();
 
+            int lineCount = 0;
+            String [] httpString = null;
             String line = in.readLine();
-            while( line != null ){
-                int count = 0;
-                //while (count < 2) {
-                System.out.println( line );
+            //while (line != null) {
+            while (lineCount < 1) {
+                httpString = line.split(" ");
+                System.out.println(host+ " supports " + httpString[0]);
                 line = in.readLine();
-                //count ++;
-                //count ++;
+                lineCount++;
             }
 
             in.close();
             out.close();
             socket.close();
-        }
-        catch( Exception e ) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
